826994af40d2990a593478a0d36018dad5eb9cfb
HelloSegun <segunkuye@hotmail.com>
2015-07-14 08:07:12 +0100

    Update on 14.07.2015 08:07

---
+++ src/javarecup/DateDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javarecup;
+
+import java.util.Date;
+
+/**
+ *
+ * @author segun
+ * Java - Date & Time
+ * http://www.tutorialspoint.com/java/java_date_time.htm
+ */
+public class DateDemo {
+    
+    public static void main(String args[]) {
+        // Instantiate a Date object
+        Date date = new Date();
+        
+        // display time and date using toSting()
+        System.out.println(date.toString());
+    }
+}
