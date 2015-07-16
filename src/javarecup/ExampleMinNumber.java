c78c8413b40778c57eb4741fdee7b6ad8c5e7fa0
KuyeS10 <KuyeS10@DEV-SEGUN.hubdev.interoute.com>
2015-07-16 16:47:09 +0100

    Practice from office 16/07/2015 Update

---
+++ src/javarecup/ExampleMinNumber.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javarecup;
+
+/**
+ *
+ * @author KuyeS10
+ * Java - Method
+ * http://www.tutorialspoint.com/java/java_methods.htm
+ */
+public class ExampleMinNumber {
+    
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 6;
+        int c = minFunction(a, b);
+        System.out.println("Minimum Value = " + c);
+    }
+    
+    public static int minFunction(int n1, int n2) {
+        int min;
+        if (n1 > n2) {
+            min = n2;
+        } else {
+            min = n1;
+        }
+        return min;
+    }
+}
---
+++ src/javarecup/ExampleVoid.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javarecup;
+
+/**
+ *
+ * @author KuyeS10
+ * Java - Method
+ * http://www.tutorialspoint.com/java/java_methods.htm
+ */
+public class ExampleVoid {
+    
+}
