73a70f1d17efd2496dc6aea0d819c372c5befcd3
HelloSegun <segunkuye@hotmail.com>
2015-07-29 01:11:53 +0100

    Update on 29.07.2015 01:11

---
+++ src/javarecup/TestDog.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javarecup;
+
+/**
+ *
+ * @author segun
+ */
+class Animal {
+    public void move() {
+        System.out.println("Animal can move");
+    }
+}
+
+class Dog extends Animal {
+    public void move() {
+        System.out.println("Dog can walk and run");
+    }
+}
+
+public class TestDog {
+    public static void main(String[] args) {
+        Animal a = new Animal(); // Animal reference and object
+        Animal b = new Dog(); //Animal reference but Dog object
+        
+        a.move(); //runs the method in the Animal class
+        b.move(); //runs the method in the Dog class
+    }
+}
